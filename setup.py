56931c373c00add0fc3d9696e6be790e97971a43
Fabien Parent <fparent@baylibre.com>
2021-08-23 19:39:11 +0200

    Initial commit
    Based on bootrom-tool commit 25770c23c00d0922d161dfd0d1567b2a9de0ed36
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: MIT
+# Copyright 2021 BayLibre, SAS.
+
+import setuptools
+
+setuptools.setup(
+    name="aiot-bootrom",
+    version="1.1.0",
+    author="Fabien Parent",
+    author_email="fparent@baylibre.com",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={
+        "": ["bin/bootrom-tool"],
+        "": ["bin/bootrom-tool.exe"],
+    },
+    entry_points={
+        'console_scripts': [
+            'aiot-bootrom=aiot_bootrom.bootrom:main',
+        ]},
+);
