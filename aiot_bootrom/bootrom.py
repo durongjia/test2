c640944f1ed58ee3242e392bc7b8721c9dbc49d6
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2022-05-25 17:13:40 +0200

    Revert "bootrom.py: add retries"
    This reverts commit cc01603eff9943b5e04d499a33afc3d40e3e6e20.
    
    The current fix is un-sufficient, and we have a solution in aiot-flash
    for this:
    ccadf496e271 ("flash: wait until the udev changed device permissions")
    
    Also see:
    https://gitlab.com/mediatek/aiot/bsp/aiot-bootrom/-/merge_requests/11#note_959974575
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- aiot_bootrom/bootrom.py
+++ aiot_bootrom/bootrom.py
@@ -6,7 +6,6 @@ import pkg_resources
 import platform
 import subprocess
 import sys
-import time
 
 def main():
     bin_name = 'bin/bootrom-tool'
@@ -16,11 +15,7 @@ def main():
     binary = pkg_resources.resource_filename('aiot_bootrom', bin_name)
     sys.argv[0] = binary
 
-    for _ in range(5):
-        try:
-            subprocess.run(sys.argv, check=True)
-        except KeyboardInterrupt:
-            pass
-        except Exception as expt:
-            print("aiot_bootrom failure: ", expt)
-            time.sleep(0.2)
+    try:
+        subprocess.run(sys.argv, check=True)
+    except KeyboardInterrupt:
+        pass
